daf5b2ccc6eab4d3f41c760cc88224664025b00e
beer-psi <lacvtg.a1.2023@gmail.com>
2025-09-26 02:04:07 +0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use audiopus::{coder::Decoder, Channels, SampleRate};
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn create_opus_decoder() {
+    Decoder::new(SampleRate::Hz48000, Channels::Stereo);
+}
+
+/// A Python module implemented in Rust.
+#[pymodule(name = "_native")]
+fn namespace_package_reproduction(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(create_opus_decoder, m)?)?;
+    Ok(())
+}
